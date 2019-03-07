ce1ad1f2f1b4586f4af15a3908ccc85f83aaceca
analhi7 <analhi-e-m7@gmail.com>
2019-03-07 13:40:59 -0600

    Actuliza sesion facebook

--- src/main.js
+++ src/main.js
@@ -20,6 +20,28 @@ const logoFacebok = document.getElementById("logo-fb")
 
 
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+   FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+
+    const response= {
+        status: 'connected',
+        authResponse: {
+            accessToken: '...',
+            expiresIn:'...',
+            signedRequest:'...',
+            userID:'...'
+        }
+    };
+});
+
 if (location.pathname === "/src/views/registro.html") {
   register.addEventListener("click", () => {
       const mailUser = mail.value;
