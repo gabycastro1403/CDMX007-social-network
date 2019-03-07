4fbf81d40290ec84afe47eaa9d7a1fc05bfcabd6
analhi7 <analhi-e-m7@gmail.com>
2019-03-07 11:32:48 -0600

    Actualiza inicio de sesion con facebook

--- src/main.js
+++ src/main.js
@@ -47,30 +47,38 @@ logoGoogle.addEventListener("click", () => {
   firebase.auth().signInWithRedirect(baseProvider).then(result => console.log(result.message))
   .catch (e => console.log(e.message));
   
-})
+});
+
+logoFacebok.addEventListener("click", () =>{
+    alert("holi")
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithRedirect(provider).then(function(result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    });
+    
+    
+   
 
 
 }
 
-logoFacebok.addEventListener("click", () =>{
-const provider = new firebase.auth.FacebookAuthProvider();
-firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      var token = result.credential.accessToken;
-    }
-    var user = result.user;
-  }).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-  });
 
-})
-  
-    
-    
-   
 
 
 
