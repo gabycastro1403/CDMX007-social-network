67fa350eb3a9d00024136c6e312a3def4250f6ae
Ed <ed@MacBook-Pro-de-Ed.local>
2019-03-08 17:05:33 -0600

    Subiendo camios en el policia y facebook verificado y 100% aprobado

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 //eventos de DO
 
 var config = {
-    apiKey: "AIzaSyAylC42T8eTT0b7dsg082rrZLf1ooaxwj4",
-    authDomain: "red-social-dolce.firebaseapp.com",
-    databaseURL: "https://red-social-dolce.firebaseio.com",
-    storageBucket: "red-social-dolce.appspot.com",
+  apiKey: "AIzaSyAylC42T8eTT0b7dsg082rrZLf1ooaxwj4",
+  authDomain: "red-social-dolce.firebaseapp.com",
+  databaseURL: "https://red-social-dolce.firebaseio.com",
+  storageBucket: "red-social-dolce.appspot.com",
 };
 firebase.initializeApp(config);
 
@@ -22,86 +22,91 @@ const logoFacebok = document.getElementById("logo-fb")
 
 if ((location.href.match(/registro.html$/gm))) {
   register.addEventListener("click", () => {
-      const mailUser = mail.value;
-      const passwordUser = password.value;
-      console.log(mailUser, passwordUser)
-      const auth = firebase.auth();
-      auth.createUserWithEmailAndPassword(mailUser, passwordUser);
-      firebase.auth().onAuthStateChanged(firebaseUser => {
-        if (firebaseUser) {
-          if(!location.href.match(/muro.html$/gm)){
-            location.replace('muro.html');
-          } 
-        } else {
-            alert("No reegistrado");
-    
+    const mailUser = mail.value;
+    const passwordUser = password.value;
+    console.log(mailUser, passwordUser)
+    const auth = firebase.auth();
+    auth.createUserWithEmailAndPassword(mailUser, passwordUser);
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        if (!location.href.match(/muro.html$/gm)) {
+          location.replace('muro.html');
         }
+      } else {
+        alert("No reegistrado");
+
+      }
     })
 
   });
-}else if ((location.href.match(/muro.html$/gm))){
+} else if ((location.href.match(/muro.html$/gm))) {
 
-  
-logOut.addEventListener('click', () =>{
-  alert("si funciono");
-  firebase.auth().signOut();
-  location.replace('../index.html');
-});
 
-}else{
+  logOut.addEventListener('click', () => {
+    alert("si funciono");
+    firebase.auth().signOut();
+    location.replace('../index.html');
+  });
+
+} else {
   login.addEventListener("click", () => {
     const mailUser = mailLogin.value;
     const passwordUser = passLogin.value;
     const auth = firebase.auth();
     const promise = auth.signInWithEmailAndPassword(mailUser, passwordUser);
-     promise
-     .then(location.replace("views/muro.html"))
-     .catch(e => alert(e.message));
-});       
+    promise
+      .then(location.replace("views/muro.html"))
+      .catch(e => alert(e.message));
+  });
 
-logoGoogle.addEventListener("click", () => {
-  alert("Si funciono");
-  const baseProvider = new firebase.auth.GoogleAuthProvider()
-  firebase.auth().signInWithRedirect(baseProvider)
-  .catch (e => console.log(e.message));
- 
-})
+  logoGoogle.addEventListener("click", () => {
+    alert("Si funciono");
+    const baseProvider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithRedirect(baseProvider)
+      .catch(e => console.log(e.message));
 
- 
+  })
 
-logoFacebok.addEventListener("click", () =>{
+
+
+  logoFacebok.addEventListener("click", () => {
     alert("holi")
     const provider = new firebase.auth.FacebookAuthProvider();
-    firebase.auth().signInWithRedirect(provider).then(function(result) {
-        if (result.credential) {
-          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-          var token = result.credential.accessToken;
-          // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+    firebase.auth().signInWithRedirect(provider).then(function (result) {
+      if (result.credential) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
         // ...
-      });
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
     });
-  } 
+  });
+}
 
 
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-      if(!location.href.match(/muro.html$/gm)){
-        location.replace('views/muro.html');
-      } 
-    } else {
-        alert("No reegistrado");
-
+  if (firebaseUser) {
+    console.log(firebaseUser);
+    if (!location.href.match(/muro.html$/gm)) {
+      location.replace('views/muro.html');
     }
+  } else {
+    if (location.href.match(/muro.html$/gm)) {
+      location.replace('../index.html');
+    }
+    console.log(location.pathname)
+    alert("No reegistrado");
+
+  }
 })
