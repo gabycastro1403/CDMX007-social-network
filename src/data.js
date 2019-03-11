ddee8c8a96f3199dc2341d743c91d9057abb0b1c
Ed <ed@MacBook-Pro-de-Ed.local>
2019-03-11 10:38:48 -0600

    Subiendo cambios en main y en data. en main guarda los datos y en data recupera los datos del formulario y los imprime en ewl muro

--- src/data.js
+++ src/data.js
@@ -1,1 +1,18 @@
 
+if ((location.href.match(/muro.html$/gm))){
+  
+  const perfil = document.getElementById("perfil");
+  const data = document.getElementById("data-user");
+
+  perfil.addEventListener("click", () => {
+    var db = firebase.firestore();
+  
+  db.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        let dataUser=`<div>${doc.data().speciality}</div>`
+        data.insertAdjacentHTML("beforeend", dataUser);
+
+    });
+});
+  })
+}
