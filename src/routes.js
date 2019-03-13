d2d9933949643fc76793cab2fc773477c8398a65
elenore29 <elenore.nava@gmail.com>
2019-03-13 14:40:04 -0600

    Imprementamos SPA y creamos el controlador y las cargas para llamar a las funciones

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+(function(window, document){
+    library.getID('view').enroute()
+    .route('/', './views/login.html', 'controlador', window.controlador.iniciarSesion)
+    .route('/registro', './views/registro.html', 'controlador', window.controlador.registro)
+    .route('/muro', 'views/muro.html', 'controlador', window.controlador.cerrarSesion)
+
+})(window, document);
+
