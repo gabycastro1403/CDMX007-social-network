087aaf5241adb8e8269a89545976d5764b2ee22a
Ed <ed@MacBook-Pro-de-Ed.local>
2019-03-19 15:11:04 -0600

    Subiendo verificaion de ususario con correo electronico y hash en luhar de location.replace

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@
 
   registro: ()=> {
     const register=  document.getElementById('register'); 
+    
     register.addEventListener('click', () => {
       const mailUser = document.getElementById('mail').value;
       const passwordUser = document.getElementById('password').value;
@@ -18,11 +19,14 @@
       const settings = { timestampsInSnapshots: true};
       db.settings(settings);
 
+
       if (passwordUser === passwordConfirmation){
       auth.createUserWithEmailAndPassword(mailUser, passwordUser);
       }else if(passwordUser != passwordConfirmation){
         alert("La contraseña debe de ser igual");
       };
+      
+        
 
       if(mailUser!='' && nameUser != '' && passwordUser != '' && passwordConfirmation != '' && lastNameUser!=''&&passwordUser === passwordConfirmation ){
         db.collection('users').add({
@@ -45,25 +49,93 @@
     }
     
 
-      // if(emailVerified == true){
-      //   location.replace('#/muro');
-      // }else if(emailVerified == false){
-      //   location.replace('#/login');
-      // };
-
-      firebase.auth().onAuthStateChanged(firebaseUser => {
-        if (firebaseUser) {
-          if (!location.href.match('#/muro')) {
-            location.replace('#/muro');
-
-          }
-        } else {
-          if (location.href.match('#/login')) {
-            location.replace('#/login');
-          };
-        };
+    const verify = () =>{
+      var user = firebase.auth().currentUser;
+      user.sendEmailVerification().then(function() {
+       // Email sent.
+       if (user.emailVerified === true)
+       console.log('Send an email')
+        location.hash='/muro'
+      }).catch(function(error) {
+       // An error happened.
+       console.log(error);
       });
-    })
+    }
+    verify(mailUser);
+
+    db.collection("users").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const mailStorage = localStorage.getItem('UID');
+        if (doc.id == mailStorage){
+          let nameNull =doc.data().first;
+          let lastNull = doc.data().last;
+          localStorage.setItem("lastNull", lastNull);
+         localStorage.setItem("nameNull",nameNull);
+        }
+        });
+      });
+    });
+  },
+
+  iniciarSesion: () => {
+    const login= document.getElementById('button-login');
+    const logoGoogle= document.getElementById('logo-google');
+    const logoFacebok= document.getElementById('logo-fb');
+    const mailLogin= document.getElementById('mail-login');
+    const passLogin= document.getElementById('password-login');
+      
+
+    login.addEventListener('click', () => {
+      console.log(location.hash)
+      const mailUser = mailLogin.value;
+      const passwordUser = passLogin.value;
+      const auth = firebase.auth();
+      auth.signInWithEmailAndPassword(mailUser, passwordUser);
+      
+    });
+
+    logoGoogle.addEventListener('click', () => {
+      const baseProvider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(baseProvider)
+        .catch(e => console.log(e.message));
+        
+    });
+
+    logoFacebok.addEventListener('click', () => {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      firebase.auth().signInWithRedirect(provider).then(function (result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    });
+
+     //muro
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if (firebaseUser) {
+        console.log(firebaseUser)
+           console.log(firebaseUser+ 'hay usuario')
+           location.hash = '/muro';
+         }else{
+           // console.log('no hay usuario')
+           //  location.hash = '/login';
+      }
+    })  
+    
+     
   },
  
   cerrarSesion : () => {
@@ -74,22 +146,9 @@
     const post = document.getElementById("post");
     const newPost = document.getElementById('new-post');
     const perfilUsuario = document.getElementById("perfil-usuario");
-
     var db = firebase.firestore();
       const settings = { timestampsInSnapshots: true};
       db.settings(settings);
-
-      db.collection("users").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          const mailStorage = localStorage.getItem('UID');
-          if (doc.id == mailStorage){
-            let nameNull =doc.data().first;
-            let lastNull = doc.data().last;
-            localStorage.setItem("lastNull", lastNull);
-           localStorage.setItem("nameNull",nameNull);
-          }
-          });
-        });
 
       var user = firebase.auth().currentUser;
       if (user != null) {
@@ -127,12 +186,6 @@
     })};
     
     printAll();
-    
-
-    logOut.addEventListener('click', () => {
-      firebase.auth().signOut();
-      location.replace('#/login');
-    })
 
     post.addEventListener('click', ()=> {
       const publication2 = publication.value;
@@ -196,63 +249,17 @@
         });
       }
     });
+
+    logOut.addEventListener('click', () => {
+      console.log('djsfh')
+      firebase.auth().signOut();
+        console.log("Usuario fuera");
+        location.hash = '/login';
+    })
+
+    
   },
 
-  iniciarSesion: () => {
-    const login= document.getElementById('button-login');
-    const logoGoogle= document.getElementById('logo-google');
-    const logoFacebok= document.getElementById('logo-fb');
-    const mailLogin= document.getElementById('mail-login');
-    const passLogin= document.getElementById('password-login');
-
-    login.addEventListener('click', () => {
-      console.log(location.hash)
-      const mailUser = mailLogin.value;
-      const passwordUser = passLogin.value;
-      const auth = firebase.auth();
-      const promise = auth.signInWithEmailAndPassword(mailUser, passwordUser);
-      promise
-        .then(location.replace('#/muro'))
-        .catch(e => alert(e.message));
-    });
-
-    logoGoogle.addEventListener('click', () => {
-      const baseProvider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithRedirect(baseProvider)
-        .catch(e => console.log(e.message));
-    });
-
-    logoFacebok.addEventListener('click', () => {
-      const provider = new firebase.auth.FacebookAuthProvider();
-      firebase.auth().signInWithRedirect(provider).then(function (result) {
-        if (result.credential) {
-          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-          var token = result.credential.accessToken;
-          // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-      }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-    })
-      firebase.auth().onAuthStateChanged(firebaseUser => {
-        if (firebaseUser) {
-          console.log(firebaseUser);
-          if (!location.href.match('#/muro')) {
-            location.replace('#/muro');
-          }
-        } else {
-          if (location.href.match('#/login')) {
-          }
-        }
-      })  
-  }, 
-};
+  
+  
+}
